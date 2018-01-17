a7acdcacac6700e79e971c28cbeacd211dae1eb7
rawlesson <35452403+rawlesson@users.noreply.github.com>
2018-01-18 04:20:13 +0700

    Add files via upload

---
+++ todobot.py
@@ -0,0 +1,100 @@
+from dbhelper import DBHelper
+
+db = DBHelper()
+
+import json 
+import requests
+import time
+import urllib
+
+TOKEN = "458860089:AAHSd7KhdNizBZxoHUWbkHJEcuNzLEX5k2I"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+# downloads the content from url to give string
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+# get the strings response as above and parese to python
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+# call the API
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+#function to calculate the highest ID of all update
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+#loop through update and grab the text and chat component
+def handle_updates(updates):
+    for update in updates["result"]:
+        text = update["message"]["text"]
+        chat = update["message"]["chat"]["id"]
+        items = db.get_items(chat)
+        if text == "/done":
+            keyboard = build_keyboard(items)
+            send_message("Select an item to delete", chat, keyboard)
+        elif text == "/start":
+            send_message("Welcome to your personal To Do list. Send any text to me and I'll store it as an item. Send /done to remove items", chat)
+        elif text.startswith("/"):
+            continue
+        elif text in items:
+            db.delete_item(text, chat)
+            items = db.get_items(chat)
+            keyboard = build_keyboard(items)
+            send_message("Select an item to delete", chat, keyboard)
+        else:
+            db.add_item(text, chat)
+            items = db.get_items(chat)
+            message = "\n".join(items)
+            send_message(message, chat)
+
+# to get the chat ID and message text
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+# sending text message
+def send_message(text, chat_id, reply_markup=None):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    if reply_markup:
+        url += "&reply_markup={}".format(reply_markup)
+    get_url(url)
+
+#add keyboard
+def build_keyboard(items):
+    keyboard = [[item] for item in items]
+    reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
+    return json.dumps(reply_markup)    
+
+# text, chat = get_last_chat_id_and_text(get_updates())
+# send_message(text, chat)
+
+def main():
+    db.setup()
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            handle_updates(updates)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
